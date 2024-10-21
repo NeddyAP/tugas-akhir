c9c0cbca4ed55db1f2f0e8abc76546cffd7cf982
neddy1298 <neddy1298@gmail.com>
2024-10-21 16:24:36 +0700

    Refactor Footer component to improve layout and add Filkom logo

--- resources/js/Layouts/Footer.jsx
+++ resources/js/Layouts/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from '@inertiajs/react';
+import filkomLogo from '../../images/filkom.png';
 
 const SocialIcon = React.memo(({ href, icon }) => (
     <a href={href} target="_blank" rel="noopener noreferrer" className="mr-2">
@@ -8,9 +9,9 @@ const SocialIcon = React.memo(({ href, icon }) => (
 ));
 
 const FooterSection = React.memo(({ title, links }) => (
-    <div className="widget mb-6">
-        <h4 className="widget-title mb-3 font-bold">{title}</h4>
-        <ul className="list-none p-0">
+    <div className="mb-6 widget">
+        <h4 className="mb-3 font-bold widget-title">{title}</h4>
+        <ul className="p-0 list-none">
             {links.map((link, index) => (
                 <li key={index} className="mb-2">
                     <Link href={link.href} className="text-blue-500 hover:underline">{link.text}</Link>
@@ -22,9 +23,9 @@ const FooterSection = React.memo(({ title, links }) => (
 
 const ContactInfo = React.memo(() => (
     <div className="widget">
-        <h4 className="widget-title mb-3 font-bold">Kontak</h4>
+        <h4 className="mb-3 font-bold widget-title">Kontak</h4>
         <address className="mb-2">Jl. Tol Ciawi No. 1, Ciawi-Bogor, Jawa Barat, Indonesia.</address>
-        <a href="mailto:filkom@unida.ac.id" className="text-blue-500 block mb-1">filkom@unida.ac.id</a>
+        <a href="mailto:filkom@unida.ac.id" className="block mb-1 text-blue-500">filkom@unida.ac.id</a>
         <span>02518240773</span>
     </div>
 ));
@@ -38,7 +39,7 @@ const socialLinks = [
 ];
 
 const SocialIcons = React.memo(() => (
-    <nav className="nav social flex justify-center md:justify-start space-x-4">
+    <nav className="flex justify-center space-x-4 nav social md:justify-start">
         {socialLinks.map((social, index) => (
             <SocialIcon key={index} href={social.href} icon={social.icon} />
         ))}
@@ -49,14 +50,14 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="container mx-auto px-4 md:px-8 lg:px-16 py-8 md:py-20">
+        <footer className="container px-4 py-8 mx-auto md:px-8 lg:px-16 md:py-20">
             <div className="container mx-auto">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
                     <div className="col-span-1 md:col-span-2 lg:col-span-1">
-                        <div className="widget flex flex-col items-center md:items-start">
+                        <div className="flex flex-col items-center widget md:items-start">
                             <img
                                 className="mb-4 w-full max-w-[300px] h-auto object-contain"
-                                src="https://filkom.unida.ac.id/storage/logo/1694631383.png"
+                                src={filkomLogo}
                                 alt="Fakultas Ilmu Komputer"
                             />
                             <SocialIcons />
@@ -78,7 +79,7 @@ const Footer = () => {
                     <ContactInfo />
                 </div>
 
-                <div className="flex justify-center mt-8 pt-5 border-t">
+                <div className="flex justify-center pt-5 mt-8 border-t">
                     <p className="text-sm text-gray-600">© {currentYear} made with ❤️ Djuanda University.</p>
                 </div>
             </div>
