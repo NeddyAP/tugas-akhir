6ee1e5e157cb60c15f54f027b9faa3569fe2e3ab
neddy1298 <neddy1298@gmail.com>
2024-10-28 19:51:48 +0700

    Refactor AdminLayout component to include toast notifications for success and error messages

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -1,8 +1,27 @@
-import { Head } from '@inertiajs/react';
+import { Head, usePage } from '@inertiajs/react';
 import AdminNavbar from '../Components/Admin/AdminNavbar';
 import AdminSidebar from '../Components/Admin/AdminSidebar';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import React, { useEffect } from 'react';
 
 export default function AdminLayout({ children, title, currentPage }) {
+    const { flash } = usePage().props.flash;
+
+    useEffect(() => {
+        if (flash && flash.message) {
+            if (flash.type === "success") {
+                toast.success(flash.message, {
+                    className: 'foo-bar'
+                });
+            } else if (flash.type === "error") {
+                toast.error(flash.message, {
+                    className: 'foo-bar'
+                });
+            }
+        }
+    }, [flash]);
+
     return (
         <div className="flex min-h-screen bg-gray-50">
             <Head title={title} />
@@ -13,6 +32,7 @@ export default function AdminLayout({ children, title, currentPage }) {
 
                 <main className="flex-1 p-8">
                     {children}
+                    <ToastContainer position="top-right" autoClose={3000} />
                 </main >
             </div >
         </div >
