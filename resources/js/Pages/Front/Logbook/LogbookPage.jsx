25325992c333339b8600c4d1e0fa15ebfcc13eb4
neddy1298 <neddy1298@gmail.com>
2024-11-20 15:48:43 +0700

    Change month format in date display to 'long' for improved readability in LogbookPage

--- resources/js/Pages/Front/Logbook/LogbookPage.jsx
+++ resources/js/Pages/Front/Logbook/LogbookPage.jsx
@@ -8,7 +8,7 @@ const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString('id-ID', {
         day: '2-digit',
-        month: 'short',
+        month: 'long',
         year: 'numeric',
     });
 };
