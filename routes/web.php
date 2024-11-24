7b00ccdd88c54582bf46e5813b6da231e9bf6310
neddy1298 <neddy1298@gmail.com>
2024-11-24 12:50:38 +0700

    Add Laporan routes and file retrieval functionality with authentication

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\BimbinganController as AdminBimbinganController;
 use App\Http\Controllers\Admin\ExportController as AdminExportController;
 use App\Http\Controllers\Admin\InformationController as AdminInformationController;
 use App\Http\Controllers\Admin\LogbookController as AdminLogbookController;
+use App\Http\Controllers\Admin\LaporanController  as AdminLaporanController;
 use App\Http\Controllers\BimbinganController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\LogbookController;
@@ -15,6 +16,7 @@ use App\Models\Question;
 use App\Models\Tutorial;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Storage;
 
 Route::inertia('/', 'Front/Home/HomePage', [
     'canLogin' => Route::has('login'),
@@ -25,6 +27,14 @@ Route::inertia('/pedomans', 'Front/Pedoman/PedomanPage', [
     'canLogin' => Route::has('login'),
     'panduans' => Panduan::select('title', 'description', 'file')->latest()->get(),
 ])->name('pedomans.index');
+
+Route::get('files/laporan/{filename}', function ($filename) {
+    $path = 'laporans/' . $filename;  // Keep consistent path structure
+    if (!Storage::disk('private')->exists($path)) {
+        abort(404);
+    }
+    return response()->file(storage_path('app/private/' . $path));
+})->name('files.laporan')->middleware('auth');
 
 Route::middleware('auth')->group(function () {
 
@@ -47,14 +57,14 @@ Route::middleware('auth')->group(function () {
 
         Route::resource('logbooks', AdminLogbookController::class);
         Route::resource('bimbingans', AdminBimbinganController::class);
+        Route::resource('laporans', AdminLaporanController::class);
 
         Route::get('users/export', [AdminExportController::class, 'export'])
             ->name('users.export');
         Route::resource('users', UserController::class);
-        Route::get('/laporans', [LaporanController::class, 'index'])->name('laporans.index');
 
         Route::resource('informations', AdminInformationController::class);
     });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
